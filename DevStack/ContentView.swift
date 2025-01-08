1895903c559d5195d3e9b6d4c70419421f358aa3
ivansaul <saul_2503@hotmail.com>
2025-01-08 13:13:43 -0500

    chore: initial project setup

---
+++ DevStack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DevStack
+//
+//  Created by @ivansaul on 1/8/25.
+//
+//  https://github.com/ivansaul
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevStack/DevStackApp.swift
@@ -0,0 +1,19 @@
+//
+//  DevStackApp.swift
+//  DevStack
+//
+//  Created by @ivansaul on 1/8/25.
+//
+//  https://github.com/ivansaul
+//
+
+import SwiftUI
+
+@main
+struct DevStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
