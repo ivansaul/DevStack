b788425fde12b32b43dccaeebaac8c5ac9b12544
ivansaul <saul_2503@hotmail.com>
2025-05-06 01:58:32 -0500

    refactor: simplify preview data loading

--- DevStack/Sources/Shared/Mocks/PreviewData.swift
+++ DevStack/Sources/Shared/Mocks/PreviewData.swift
@@ -10,22 +10,35 @@
 import Foundation
 
 extension [Cheatsheet] {
-    static var preview: [Cheatsheet] {
-        let url = Bundle.main.url(forResource: "cheatsheets", withExtension: "json")!
-        let data = try! Data(contentsOf: url)
-        let result = try! JSONDecoder().decode([Cheatsheet].self, from: data)
-        return result
-    }
+    static let preview = cheatsheets
 }
 
 extension Cheatsheet {
-    static var preview: Cheatsheet {
-        return [Cheatsheet].preview.first!
-    }
+    static let preview = [Self].preview.first!
 }
 
-extension CheatsheetsViewModel {
-    static let preview: CheatsheetsViewModel = .init(
-        cheatsheetsDataService: MockCheatsheetsDataService()
-    )
+extension SectionSheet {
+    static let preview = [Cheatsheet].preview.first!.sections!.first!
+}
+
+private let cheatsheets: [Cheatsheet] = load("cheatsheets.json")
+
+private func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else { fatalError("Couldn't find \(filename) in main bundle.") }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
 }
